98f92c0360c8d0ee65ec42d466c758789ff2f83f
Lau <arinel88@gmail.com>
2020-11-03 01:09:31 +0100

    Creato seeder e utilizzato Faker

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+use Faker\Generator as Faker;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 20; $i++) {
+
+            $newComic = new Comic();
+            $newComic->title = $faker->text(50);
+                if(rand(0,1) == 1) {
+                    $newComic->original_title = $faker->text(50);
+                };
+            $newComic->story_by = $faker->name();
+            $newComic->draws_by = $faker->name();            
+            $newComic->country = $faker->state();
+            $newComic->volumes = $faker->numberBetween(1, 250);
+            $newComic->edition = $faker->company();
+            $newComic->reading = (rand(0,1) == 1) ? "LT" : "RT";
+            $newComic->color = rand(0, 1);
+            $newComic->release = $faker->date();
+            $newComic->price = $faker->randomFloat(2, 0, 800);
+            $newComic->cover = "https://picsum.photos/200/300";
+            $newComic->save();
+        }
+    }
+}
